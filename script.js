8a0445a10cc635c08a04ed48434dcee0bbbad6c7
sejalbahekar123 <sejalbahekar123@gmail.com>
2026-02-18 15:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-// Your script here.
+function populateVoices() {
+  voices = this.getVoices();
+
+  voicesDropdown.innerHTML = voices
+    .map(voice => `
+      <option value="${voice.name}">
+        ${voice.name} (${voice.lang})
+      </option>
+    `)
+    .join('');
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+
+  // Restart speech if already speaking
+  if (speechSynthesis.speaking) {
+    toggle(true);
+  }
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+
+  // Restart speech dynamically if speaking
+  if (speechSynthesis.speaking) {
+    toggle(true);
+  }
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+
+  if (startOver) {
+    // Prevent speaking empty text
+    if (!msg.text.trim()) {
+      alert("Please enter some text.");
+      return;
+    }
+    speechSynthesis.speak(msg);
+  }
+}
+
+// Initialize default text
+msg.text = document.querySelector('[name="text"]').value;
+
+// Event Listeners
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+voicesDropdown.addEventListener('change', setVoice);
+
+options.forEach(option => option.addEventListener('input', function () {
+  if (this.name === 'text') {
+    msg.text = this.value;
+  } else {
+    setOption.call(this);
+  }
+}));
+
+speakButton.addEventListener('click', () => toggle(true));
+stopButton.addEventListener('click', () => toggle(false));
